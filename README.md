9f73dcd0d1e6608e335ead6b7134170bf20dabff
Madhumita Boddapati <madhumita.boddapati@opengov.com>
2019-11-04 13:44:13 -0800

    adding restart script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wf-restarts
+
+Go script to connect to workforce kubernetes pods to recycle them.****
---
+++ internal/wf-restarts/restart.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	appsv1 "k8s.io/api/apps/v1"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/util/retry"
+	//
+	// Uncomment to load all auth plugins
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+)
+
+// list pods in my local k8s cluster
+func listPods() {
+}
